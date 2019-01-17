3bd3c65a07ed3fecebc8f2bbaf4714c35b195fe9
Lukas Levin <lukas@wgtwo.com>
2019-01-17 16:00:12 +0100

    fix ipv4

--- src/pcrf/pcrf_gx_path.c
+++ src/pcrf/pcrf_gx_path.c
@@ -1830,6 +1830,7 @@ static status_t update_qos(
 static struct sess_state * get_gx_state()
 {   
     char buf[CORE_ADDRSTRLEN];
+    int i;
     struct sess_state *sess_ptr = new_state((os0_t) "pcrf.open-ims.test;1547586413;1;CCR_SESSION");
     sess_ptr->cc_request_type = (c_uint32_t)1;
     sess_ptr->peer_host = (os0_t) "pcrf.open-ims.test";
@@ -1838,8 +1839,8 @@ static struct sess_state * get_gx_state()
     sess_ptr->ipv4 = (c_uint8_t)1;
     sess_ptr->ipv6 = (c_uint8_t)0;
     sess_ptr->reserved = (c_uint8_t)0;
-    inet_pton(AF_INET, "45.45.0.3", &sess_ptr->addr);
-    printf("setting addr4: %s\n", INET_NTOP(sess_ptr->addr, buf));
+    i = inet_pton(AF_INET, "45.45.0.3", &sess_ptr->addr);
+    printf("setting addr4: %s\nSuccess: %d\n", INET_NTOP(sess_ptr->addr, buf), i);
     c_uint8_t ipv6addr[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     memcpy(sess_ptr->addr6, ipv6addr, IPV6_LEN);
     clock_gettime(CLOCK_REALTIME, &sess_ptr->ts);
