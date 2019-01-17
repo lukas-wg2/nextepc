504747813301b75c771e7795a57c39752372a7e4
Lukas Levin <lukas@wgtwo.com>
2019-01-17 12:32:42 +0100

    ip fix 2

--- src/pcrf/pcrf_gx_path.c
+++ src/pcrf/pcrf_gx_path.c
@@ -5,6 +5,8 @@
 #include "core_pool.h"
 #include "core_pkbuf.h"
 
+#include "inet.h"
+
 #include "fd/fd_lib.h"
 #include "fd/gx/gx_dict.h"
 #include "fd/rx/rx_dict.h"
@@ -1833,7 +1835,7 @@ static void get_gx_state(struct sess_state *sess_data)
     sess_data->ipv4 = (c_uint8_t)1;
     sess_data->ipv6 = (c_uint8_t)0;
     sess_data->reserved = (c_uint8_t)0;
-    core_inet_pton(AF_INET, "45.45.0.3", sess_data->addr);;
+    inet_pton(AF_INET, "45.45.0.3", sess_data->addr);;
     c_uint8_t ipv6addr[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     memcpy(sess_data->addr6, ipv6addr, IPV6_LEN);
     clock_gettime(CLOCK_REALTIME, &sess_data->ts);
